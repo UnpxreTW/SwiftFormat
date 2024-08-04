0feca106c747925689cb9e7434cfc4f475274bcf
Unpxre <mail@unpxre.me>
2024-08-04 22:16:27 +0800

    Test:

--- Plugins/SwiftFormatPlugin/Shared/Rules.swift
+++ Plugins/SwiftFormatPlugin/Shared/Rules.swift
@@ -5,30 +5,41 @@
 //  Copyright © 2023 UnpxreTW. All rights reserved.
 //
 
-// 格式規則
+/// 格式規則
 enum Rule {
 
-	// 不啟用的規則
+	/// 不啟用的規則
 	case disable(rules: String)
 
 	// MARK: - 格式規則
 
-	// 特定縮寫自動全大寫
+	/// 特定縮寫自動全大寫
 	case acronyms
 }
 
 extension Rule {
 
-	// 取得當前規則的名稱
-	//
-	// 使用 `Mirror` 取得規則名稱
+	/// 取得當前規則的名稱
+	///
+	/// 使用 `Mirror` 取得規則名稱
 	var name: String? { Mirror(reflecting: self).children.first?.label }
 
-	// 規則的設定值
-	var option: String?
+	/// 規則的設定值
+	var option: String? { 
+		guard let rule = Mirror(reflecting: self).children.first else { return nil }
+		// !!!: 一個規則只會帶一個設定值字串
+		let option = Mirror(reflecting: rule).children.first?.value
+		switch option {
+		case as String:
+			return option
+		}
+	}
 
-	// 要使用於命令行參數的字串
-	var command: [String] {}
+	/// 要使用於命令行參數的字串
+	var command: [String] {
+		guard let ruleName: String = self.name, let option = self.option else { return [] }
+		return [ruleName, option]
+	}
 }
 
 extension Rule {
