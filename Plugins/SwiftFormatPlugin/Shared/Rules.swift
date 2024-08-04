5400acbb89c422ab51c91be611c23d71f6e862ba
Unpxre <mail@unpxre.me>
2024-08-04 14:36:16 +0800

    加入定義使用的規則的檔案

---
+++ Plugins/SwiftFormatPlugin/Shared/Rules.swift
@@ -0,0 +1,51 @@
+//
+//  Rules.swift
+//  SwiftFormatPlugin
+//
+//  Copyright © 2023 UnpxreTW. All rights reserved.
+//
+
+/// 格式規則
+enum Rule {
+
+	/// 不啟用的規則
+	case disable(rules: String)
+}
+
+extension Rule {
+
+	/// 取得當前規則的名稱
+	///
+	/// 使用 `Mirror` 取得規則名稱
+	var name: String? { Mirror(reflecting: self).children.first?.label }
+
+	/// 規則的設定值
+	var option: String? { 
+		guard let rule = Mirror(reflecting: self).children.first else { return nil }
+		let option = Mirror(reflecting: rule).children.dropFirst().first?.value
+		if let option = (option as? [String: Any])?.first?.value {
+			return option as? String
+		} else {
+			return option as? String
+		}
+	}
+
+	/// 要使用於命令行參數的字串
+	var command: [String] {
+		guard let ruleName: String = self.name, let option = self.option else { return [] }
+		return ["--\(ruleName)", option]
+	}
+}
+
+extension Rule {
+
+	// 所有使用中的規則與其設定值
+	static var allRules: [Rule] = [
+		// 預設不啟用所有規則
+		.disable(rules: "all")
+	]
+
+	/// 將設定的規則轉換為命令行指令
+	static var allToCommand: [String] { Self.allRules.map { $0.command }.flatMap { $0 } }
+}
+
