c081d2907f1339c5a1cffe77c58421c6d9f06213
Unpxre <mail@unpxre.me>
2024-08-04 23:15:41 +0800

    Test

--- Plugins/SwiftFormatPlugin/Shared/Rules.swift
+++ Plugins/SwiftFormatPlugin/Shared/Rules.swift
@@ -24,6 +24,7 @@ extension Rule {
 		guard let rule = Mirror(reflecting: self).children.first else { return nil }
 		// !!!: 一個規則只會帶一個設定值字串
 		let option = Mirror(reflecting: rule).children.first?.value
+		dump(Mirror(reflecting: rule))
 		return switch option {
 		case is String: option as? String
 		default: nil
