eba612077f4cb613bead299ab9cea01c53aaf807
Unpxre <mail@unpxre.me>
2024-08-12 15:06:42 +0800

    新增規則啟用旗標 (#22)
    將規則啟用使用的旗標與附屬設定使用的旗標分離

---
+++ Plugins/SwiftFormatPlugin/Shared/RuleFlag.swift
@@ -0,0 +1,16 @@
+//
+//  RuleFlag.swift
+//  SwiftFormatPlugin
+//
+//  Copyright © 2023 UnpxreTW. All rights reserved.
+//
+
+/// 定義用於啟用指令與否的參數型態
+enum RuleFlag: String {
+
+	/// 規則啟用
+	case enable
+
+	/// 規則不啟用
+	case disable
+}
